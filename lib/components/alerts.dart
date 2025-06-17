078087d4f2da59100ef46a54a44312b4bdb3f4a6
Jiho <heyziizi@gmail.com>
2025-06-17 18:22:44 +0900

    최종 수정 0617

--- lib/components/alerts.dart
+++ lib/components/alerts.dart
@@ -85,28 +85,6 @@ class Alerts extends StatelessWidget {
   // alert 내용 정의
   AlertData getAlertData(String type) {
     switch (type) {
-      // 삭제 확인 알림
-      // case 'delete':
-      //   return AlertData(
-      //     title: '게임을 삭제하시겠습니까?',
-      //     content: '게임을 삭제하려면 \n게임 등록 시 설정한 비밀번호를 입력하세요.',
-      //     input: Container(
-      //       height: 55,
-      //       child: TextField(
-      //         maxLength: 4,
-      //         obscureText: true,
-      //         inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-      //         decoration: InputDecoration(
-      //           hintText: '예) 1234',
-      //           hintStyle: AppTextStyles.free15,
-      //           contentPadding: EdgeInsets.symmetric(vertical: 15),
-      //         ),
-      //       ),
-      //     ),
-      //     confirmTxt: '삭제',
-      //     cancelTxt: '취소',
-      //   );
-
       // 입력 내용이 없음 알림
       case 'empty':
         return AlertData(
